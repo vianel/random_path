3e40a2396b8b3edf79f3f1543abcdfee7650b93a
Vianel Rodríguez <vnl1990@gmail.com>
2020-11-12 20:58:11 -0500

    added grapher module and new drunker

--- drunk.py
+++ drunk.py
@@ -1,4 +1,5 @@
 import random
+
 class Drunk:
 
     def __init__(self, name):
@@ -11,3 +12,11 @@ class NormalDrunk(Drunk):
 
     def move(self):
        return random.choice([(0,1), (0, -1), (1,0), (-1, 0)])
+
+class WastedDrunk(Drunk):
+
+    def __init__(self, name):
+        super().__init__(name)
+
+    def move(self):
+        return (random.randint(-10,10), random.randint(-10,10))
--- random_path.py
+++ random_path.py
@@ -1,6 +1,13 @@
-from drunk import NormalDrunk
+from drunk import WastedDrunk
 from field import Field
 from coordinate import Coordinate
+from bokeh.plotting import figure, show
+
+def create_graph(x, y):
+    graph = figure(title='Random Path', x_axis_label='steps', y_axis_label='distance')
+    graph.line(x, y, legend='avg distance')
+
+    show(graph)
 
 def walking(field, drunker, steps):
     src = field.get_coordinate(drunker)
@@ -25,19 +32,23 @@ def simulate_random_path(steps, attemps, drunk_kind):
     return distances
 
 def main(distance_to_walk, attemps, drunk_kind):
+    avg_distance_per_walk = []
 
     for steps in distance_to_walk:
         distance = simulate_random_path(steps, attemps, drunk_kind)
         avg = round(sum(distance) / len(distance), 4)
         max_distance = max(distance)
         min_distance = min(distance)
+        avg_distance_per_walk.append(avg)
         print(f'{drunk_kind.__name__} has made {steps} steps')
         print(f'The avg distance is {avg} and the max distance is {max_distance} the min distance is {min_distance}')
+    create_graph(distance_to_walk, avg_distance_per_walk)
 
 
 if __name__ == '__main__':
 
-    distance_to_walk = [10, 100, 1000, 10000]
+    distance_to_walk = [10, 100, 500, 1000, 2000, 3000, 5000, 10000]
     attemps = 100
 
-    main(distance_to_walk, attemps, NormalDrunk)
+    #main(distance_to_walk, attemps, NormalDrunk)
+    main(distance_to_walk, attemps, WastedDrunk)
